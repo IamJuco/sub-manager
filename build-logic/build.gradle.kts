c599ccae590ac76500bdfc90e98023d2ea09319d
Juseonghyun <jucoding99@gmail.com>
2025-12-20 21:51:00 +0900

    gradle 라이브러리 추가 및 build-logic 세팅

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    `kotlin-dsl`
 }
+
+group = "com.juco.build_logic"
+
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+
+dependencies {
+    compileOnly(libs.android.gradle)
+    compileOnly(libs.kotlin.gradle)
+    compileOnly(libs.compose.compiler.gradle)
 }
