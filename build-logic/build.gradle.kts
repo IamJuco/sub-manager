0db46fe1ff438fe0802722401afdf78cb6a42605
Juseonghyun <jucoding99@gmail.com>
2025-12-21 17:29:08 +0900

    build-logic gradle 로직 추가

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -14,3 +14,62 @@ dependencies {
     compileOnly(libs.kotlin.gradle)
     compileOnly(libs.compose.compiler.gradle)
 }
+
+gradlePlugin {
+    plugins {
+        register("com.juco.build_logic.primitive.CommonAndroidPlugin") {
+            id = "com.juco.build_logic.primitive.common"
+            implementationClass = "com.juco.build_logic.primitive.CommonAndroidPlugin"
+        }
+
+        register("com.juco.build_logic.primitive.ComposePlugin") {
+            id = "com.juco.build_logic.primitive.compose"
+            implementationClass = "com.juco.build_logic.primitive.ComposePlugin"
+        }
+
+        register("com.juco.build_logic.primitive.HiltPlugin") {
+            id = "com.juco.build_logic.primitive.hilt"
+            implementationClass = "com.juco.build_logic.primitive.HiltPlugin"
+        }
+
+        register("com.juco.build_logic.primitive.KotlinPlugin") {
+            id = "com.juco.build_logic.primitive.kotlin"
+            implementationClass = "com.juco.build_logic.primitive.KotlinPlugin"
+        }
+
+        register("com.juco.build_logic.primitive.RetrofitPlugin") {
+            id = "com.juco.build_logic.primitive.retrofit"
+            implementationClass = "com.juco.build_logic.primitive.RetrofitPlugin"
+        }
+
+        register("com.juco.build_logic.primitive.OkHttpPlugin") {
+            id = "com.juco.build_logic.primitive.okhttp"
+            implementationClass = "com.juco.build_logic.primitive.OkHttpPlugin"
+        }
+
+        register("com.juco.build_logic.primitive.TestPlugin") {
+            id = "com.juco.build_logic.primitive.test"
+            implementationClass = "com.juco.build_logic.primitive.TestPlugin"
+        }
+
+        register("com.juco.build_logic.convention.ApplicationPlugin") {
+            id = "com.juco.build_logic.convention.application"
+            implementationClass = "com.juco.build_logic.convention.ApplicationPlugin"
+        }
+
+        register("com.juco.build_logic.convention.ComposePlugin") {
+            id = "com.juco.build_logic.convention.compose"
+            implementationClass = "com.juco.build_logic.convention.ComposePlugin"
+        }
+
+        register("com.juco.build_logic.convention.FeaturePlugin") {
+            id = "com.juco.build_logic.convention.feature"
+            implementationClass = "com.juco.build_logic.convention.FeaturePlugin"
+        }
+
+        register("com.juco.build_logic.convention.KotlinJvmPlugin") {
+            id = "com.juco.build_logic.convention.kotlin"
+            implementationClass = "com.juco.build_logic.convention.KotlinJvmPlugin"
+        }
+    }
+}
