c6f2b3baf32ad8e41813b7cb747b108cd841f4c9
Juseonghyun <jucoding99@gmail.com>
2026-01-01 23:46:06 +0900

    navigation Navigator, NavHost 설정 추가

---
+++ feature/main/src/main/java/com/juco/main/navigation/MainNavHost.kt
@@ -0,0 +1,24 @@
+package com.juco.main.navigation
+
+import android.content.Context
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import com.juco.home.navigation.homeNavGraph
+
+@Composable
+fun MainNavHost(
+    navigator: MainNavigator,
+    padding: PaddingValues,
+    onShowSnackBar: (String) -> Unit,
+    context: Context
+) {
+    NavHost(
+        navController = navigator.navController,
+        startDestination = navigator.startDestination
+    ) {
+        homeNavGraph(
+            padding = padding
+        )
+    }
+}
---
+++ feature/main/src/main/java/com/juco/main/navigation/MainNavigator.kt
@@ -0,0 +1,60 @@
+package com.juco.main.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.juco.common.navigation.MainRouteModel
+import com.juco.common.navigation.RouteModel
+import com.juco.home.navigation.navigateToHome
+import com.juco.main.component.MainMenu
+
+class MainNavigator(
+    val navController: NavHostController
+) {
+    val startDestination = MainRouteModel.Home
+    val currentDestination: NavDestination?
+        @Composable get() = navController.currentBackStackEntryAsState().value?.destination
+    val currentMenu: MainMenu?
+        @Composable get() = MainMenu.find { menu ->
+            currentDestination?.hasRoute(menu::class) == true
+        }
+
+    private val singleTopOptions = navOptions {
+        launchSingleTop = true
+        restoreState = true
+    }
+
+    fun navigate(menu: MainMenu) {
+        val navOptions = navOptions {
+            popUpTo(navController.graph.findStartDestination().id) {
+                inclusive = menu == MainMenu.HOME
+            }
+            launchSingleTop = true
+        }
+
+        when (menu) {
+            MainMenu.HOME -> navController.navigateToHome(navOptions)
+            MainMenu.SETTING -> {}
+        }
+    }
+
+    fun navigateToHome() = navController.navigateToHome(navOptions = singleTopOptions)
+
+    fun popBackStack() = navController.popBackStack()
+    fun popAllBackStack(destination: RouteModel) =
+        navController.popBackStack(destination, false)
+
+}
+
+@Composable
+fun rememberMainNavigator(
+    navController: NavHostController = rememberNavController(),
+): MainNavigator = remember(navController) {
+    MainNavigator(navController)
+}
