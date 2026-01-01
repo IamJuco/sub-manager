846a1d9d4f2174bd5e75fb975331da3b635cdbc2
Juseonghyun <jucoding99@gmail.com>
2026-01-01 23:38:51 +0900

    BottomNavigation에서 사용 될 MainMenu 추가

---
+++ feature/main/src/main/java/com/juco/main/component/MainMenu.kt
@@ -0,0 +1,34 @@
+package com.juco.main.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import com.juco.common.navigation.MainRouteModel
+import com.juco.common.navigation.RouteModel
+import com.juco.submanager.designsystem.R
+
+enum class MainMenu (
+    @DrawableRes val iconResIdFalse: Int,
+    @DrawableRes val iconResIdTrue: Int,
+    val contentDescription: String,
+    val route: RouteModel
+) {
+    HOME(
+        iconResIdFalse = R.drawable.ic_home_fill_false,
+        iconResIdTrue = R.drawable.ic_home_fill_true,
+        contentDescription = "home",
+        route = MainRouteModel.Home
+    ),
+    SETTING(
+        iconResIdFalse = R.drawable.ic_setting_fill_false,
+        iconResIdTrue = R.drawable.ic_setting_fill_true,
+        contentDescription = "setting",
+        route = MainRouteModel.Setting
+    );
+
+    companion object {
+        @Composable
+        fun find(predicate: @Composable (RouteModel) -> Boolean): MainMenu? {
+            return MainMenu.entries.find { predicate(it.route) }
+        }
+    }
+}
