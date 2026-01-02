b8d3e3c0e8fc7cf77b82153e1cfc162d01ef6cb9
Juseonghyun <jucoding99@gmail.com>
2026-01-02 20:16:25 +0900

    Color 정의 및 Theme 커스텀

--- core/designsystem/src/main/java/com/juco/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/juco/designsystem/theme/Color.kt
@@ -1,11 +1,98 @@
 package com.juco.designsystem.theme
 
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryBackgroundLight = Color(0xFFF9FAFB)
+val PrimaryBackgroundDark = Color(0xFF121212)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SecondaryBackgroundLight = Color(0xFFFFFFFF)
+val SecondaryBackgroundDark = Color(0xFF1E1E1E)
+
+val PrimaryTextLight = Color(0XFF1F2937)
+val PrimaryTextDark = Color(0xFFF3F4F6)
+
+val SecondaryTextLight = Color(0xFF6B7280)
+val SecondaryTextDark = Color(0xFF9CA3AF)
+
+val DividerLight = Color(0xFFE5E7EB)
+val DividerDark = Color(0xFF374151)
+
+val ButtonLight = Color(0xFF374151)
+val ButtonDark = Color(0xFFE5E7EB)
+
+@Stable
+class SubManagerColors(
+    primaryBackground: Color,
+    secondaryBackground: Color,
+    primaryText: Color,
+    secondaryText: Color,
+    divider: Color,
+    button: Color
+) {
+    var primaryBackground by mutableStateOf(primaryBackground)
+        private set
+    var secondaryBackground by mutableStateOf(secondaryBackground)
+        private set
+    var primaryText by mutableStateOf(primaryText)
+        private set
+    var secondaryText by mutableStateOf(secondaryText)
+        private set
+    var divider by mutableStateOf(divider)
+        private set
+    var button by mutableStateOf(button)
+        private set
+
+    fun copy(): SubManagerColors = SubManagerColors(
+        primaryBackground,
+        secondaryBackground,
+        primaryText,
+        secondaryText,
+        divider,
+        button
+    )
+
+    fun update(other: SubManagerColors) {
+        primaryBackground = other.primaryBackground
+        secondaryBackground = other.secondaryBackground
+        primaryText = other.primaryText
+        secondaryText = other.secondaryText
+        divider = other.divider
+        button = other.button
+    }
+}
+
+fun SubManagerLightColors(
+    PrimaryBackground: Color = PrimaryBackgroundLight,
+    SecondaryBackground: Color = SecondaryBackgroundLight,
+    PrimaryText: Color = PrimaryTextLight,
+    SecondaryText: Color = SecondaryTextLight,
+    Divider: Color = DividerLight,
+    Button: Color = ButtonLight
+) = SubManagerColors(
+    PrimaryBackground,
+    SecondaryBackground,
+    PrimaryText,
+    SecondaryText,
+    Divider,
+    Button
+)
+
+fun SubManagerDarkColors(
+    PrimaryBackground: Color = PrimaryBackgroundDark,
+    SecondaryBackground: Color = SecondaryBackgroundDark,
+    PrimaryText: Color = PrimaryTextDark,
+    SecondaryText: Color = SecondaryTextDark,
+    Divider: Color = DividerDark,
+    Button: Color = ButtonDark
+) = SubManagerColors(
+    PrimaryBackground,
+    SecondaryBackground,
+    PrimaryText,
+    SecondaryText,
+    Divider,
+    Button
+)
--- core/designsystem/src/main/java/com/juco/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/juco/designsystem/theme/Theme.kt
@@ -1,57 +1,52 @@
 package com.juco.designsystem.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private val LocalSubManagerColors = staticCompositionLocalOf<SubManagerColors> {
+    error("No SubManagerColors provided")
+}
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+object SubManagerTheme {
+    val colors: SubManagerColors
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalSubManagerColors.current
+}
 
 @Composable
-fun SubmanagerTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+fun ProvideSubManagerColors(
+    colors: SubManagerColors,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+    val provideColors = remember { colors.copy() }.apply { update(colors) }
+    CompositionLocalProvider(
+        LocalSubManagerColors provides provideColors,
         content = content
     )
+}
+
+@Composable
+fun SubManagerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        SubManagerDarkColors()
+    } else {
+        SubManagerLightColors()
+    }
+
+    ProvideSubManagerColors(
+        colors = colors
+    ) {
+        MaterialTheme(
+            content = content
+        )
+    }
 }
