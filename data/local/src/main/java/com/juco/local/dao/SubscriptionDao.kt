1759a709a50c1b1e4a1e0ec064b92ea1516bf596
Juseonghyun <jucoding99@gmail.com>
2026-01-06 15:24:07 +0900

    SubscriptionDao, CRUD 등 query 추가

--- data/local/src/main/java/com/juco/local/dao/SubscriptionDao.kt
+++ data/local/src/main/java/com/juco/local/dao/SubscriptionDao.kt
@@ -4,12 +4,28 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.juco.local.entity.SubscriptionEntity
+import androidx.room.Delete
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SubscriptionDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(subscription: SubscriptionEntity)
 
-    @Query("SELECT * FROM subscriptions")
-    fun getAll(): List<SubscriptionEntity>
+    @Update
+    suspend fun update(subscription: SubscriptionEntity)
+
+    @Delete
+    suspend fun delete(subscription: SubscriptionEntity)
+
+    @Query("SELECT * FROM subscriptions ORDER BY subId DESC")
+    fun getAll(): Flow<List<SubscriptionEntity>>
+
+    @Query("SELECT * FROM subscriptions WHERE subId = :subId")
+    suspend fun getSubscriptionById(subId: Long): SubscriptionEntity
+
+    @Query("SELECT EXISTS(SELECT * FROM subscriptions LIMIT 1)")
+    suspend fun hasSubscriptions(): Boolean
 }
