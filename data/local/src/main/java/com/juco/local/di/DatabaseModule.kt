f4b625e811e83cf040f551bad3781baf20abbf32
Juseonghyun <jucoding99@gmail.com>
2026-01-06 15:23:20 +0900

    User, SubscriptionDao 의존성을 Hilt Module에 주입

--- data/local/src/main/java/com/juco/local/di/DatabaseModule.kt
+++ data/local/src/main/java/com/juco/local/di/DatabaseModule.kt
@@ -2,6 +2,8 @@ package com.juco.local.di
 
 import android.content.Context
 import androidx.room.Room
+import com.juco.local.dao.SubscriptionDao
+import com.juco.local.dao.UserDao
 import com.juco.local.database.SubManagerDatabase
 import dagger.Module
 import dagger.Provides
@@ -21,5 +23,17 @@ object DatabaseModule {
             klass = SubManagerDatabase::class.java,
             name = "submanager-db"
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserDao(database: SubManagerDatabase): UserDao {
+        return database.userDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSubscriptionDao(database: SubManagerDatabase): SubscriptionDao {
+        return database.subscriptionDao()
     }
 }
