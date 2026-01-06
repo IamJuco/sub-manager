2db7f06357eaa458b5eb361f530ac58ab0c61453
Juseonghyun <jucoding99@gmail.com>
2026-01-06 15:53:32 +0900

    Local Repository 설정

--- domain/local/src/main/java/com/juco/local/repository/LocalRepository.kt
+++ domain/local/src/main/java/com/juco/local/repository/LocalRepository.kt
@@ -1,4 +1,13 @@
 package com.juco.local.repository
 
+import com.juco.local.model.Subscription
+import kotlinx.coroutines.flow.Flow
+
 interface LocalRepository {
+    suspend fun insertSubscription(subscription: Subscription)
+    suspend fun updateSubscription(subscription: Subscription)
+    suspend fun deleteSubscription(subscription: Subscription)
+    fun getAllSubscriptions(): Flow<List<Subscription>>
+    suspend fun getSubscriptionById(subId: Long): Subscription
+    suspend fun hasSubscriptions(): Boolean
 }
