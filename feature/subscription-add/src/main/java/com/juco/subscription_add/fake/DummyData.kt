068fc56c85f2ea65b44e2e01aba575960a57c0ee
Juseonghyun <jucoding99@gmail.com>
2026-01-06 20:13:31 +0900

    임시로 쓸 임시 데이터 추가

---
+++ feature/subscription-add/src/main/java/com/juco/subscription_add/fake/DummyData.kt
@@ -0,0 +1,26 @@
+package com.juco.subscription_add.fake
+
+import com.juco.local.model.Subscription
+
+val dummyData = listOf(
+    Subscription(name = "Netflix", thumbnail = "NETFLIX", price = 17000, description = "프리미엄", paymentDay = 1),
+        Subscription(name = "Youtube Premium", thumbnail = "YOUTUBE", price = 14900, paymentDay = 1),
+        Subscription(name = "Spotify", thumbnail = "SPOTIFY", price = 10900, description = "개인 요금제", paymentDay = 1),
+        Subscription(name = "Coupang Play", thumbnail = "COUPANG", price = 4990, paymentDay = 1),
+        Subscription(name = "Disney+", thumbnail = "DISNEY", price = 9900, paymentDay = 1),
+        Subscription(name = "Tving", thumbnail = "TVING", price = 13900, paymentDay = 1),
+        Subscription(name = "Wavve", thumbnail = "WAVVE", price = 10900, paymentDay = 1),
+        Subscription(name = "Watcha", thumbnail = "WATCHA", price = 7900, paymentDay = 1),
+        Subscription(name = "Apple Music", thumbnail = "APPLE", price = 8900, paymentDay = 1),
+        Subscription(name = "Apple TV+", thumbnail = "APPLE", price = 6500, paymentDay = 1),
+        Subscription(name = "Melon", thumbnail = "MELON", price = 11900, paymentDay = 1),
+        Subscription(name = "Genie", thumbnail = "GENIE", price = 8400, paymentDay = 1),
+        Subscription(name = "Flo", thumbnail = "FLO", price = 8000, paymentDay = 1),
+        Subscription(name = "Naver Plus", thumbnail = "NAVER", price = 4900, paymentDay = 1),
+        Subscription(name = "Kurly Pass", thumbnail = "KURLY", price = 4500, paymentDay = 1),
+        Subscription(name = "Smile Club", thumbnail = "GMARKET", price = 3000, paymentDay = 1),
+        Subscription(name = "Notion", thumbnail = "NOTION", price = 12000, paymentDay = 1),
+        Subscription(name = "Figma", thumbnail = "FIGMA", price = 15000, paymentDay = 1),
+        Subscription(name = "Adobe CC", thumbnail = "ADOBE", price = 62000, paymentDay = 1),
+        Subscription(name = "Chat GPT", thumbnail = "OPENAI", price = 29000, paymentDay = 1)
+    )
