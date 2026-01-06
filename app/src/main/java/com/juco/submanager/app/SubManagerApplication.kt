7e051c6ff8451bf5f704b0bc67789fdd6a696217
Juseonghyun <jucoding99@gmail.com>
2026-01-06 14:12:47 +0900

    Manifest, Activity, App에 Hilt 설정

--- app/src/main/java/com/juco/submanager/app/SubManagerApplication.kt
+++ app/src/main/java/com/juco/submanager/app/SubManagerApplication.kt
@@ -3,8 +3,10 @@ package com.juco.submanager.app
 import android.app.Application
 import com.juco.submanager.BuildConfig
 import com.juco.submanager.app.util.ReleaseTree
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class SubManagerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
