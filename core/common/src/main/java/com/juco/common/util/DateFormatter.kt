aabb7960a2579b62a190f7f88ef573885f48779e
Juseonghyun <jucoding99@gmail.com>
2026-01-07 20:52:57 +0900

    공통으로 사용할 주기로 결제 할 금액 계산기 추가

--- core/common/src/main/java/com/juco/common/util/DateFormatter.kt
+++ core/common/src/main/java/com/juco/common/util/DateFormatter.kt
@@ -13,3 +13,34 @@ fun formatDate(millis: Long): String {
         .toLocalDate()
         .format(dateFormatter)
 }
+
+fun calculateNextPaymentDate(startDateMillis: Long, type: PaymentCycleType, value: Int): Long {
+    if (type == PaymentCycleType.NONE || value <= 0) return startDateMillis
+
+    val startDate = Instant.ofEpochMilli(startDateMillis)
+        .atZone(ZoneId.systemDefault())
+        .toLocalDate()
+
+    val nextDate = when (type) {
+        PaymentCycleType.DAY -> startDate.plusDays(value.toLong())
+        PaymentCycleType.MONTH -> startDate.plusMonths(value.toLong())
+        PaymentCycleType.YEAR -> startDate.plusYears(value.toLong())
+        else -> startDate
+    }
+
+    return nextDate.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
+}
+
+data class PaymentCycle(
+    val type: PaymentCycleType = PaymentCycleType.NONE,
+    val value: Int = 0
+) {
+    fun toDisplayText(): String {
+        return when (type) {
+            PaymentCycleType.DAY -> "${value}일 마다"
+            PaymentCycleType.MONTH -> "${value}개월 마다"
+            PaymentCycleType.YEAR -> "${value}년 마다"
+            PaymentCycleType.NONE -> ""
+        }
+    }
+}
