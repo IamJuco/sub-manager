49fadb61f14520a89a7ad6852caf5c295d7c1638
Juseonghyun <jucoding99@gmail.com>
2026-01-08 18:18:32 +0900

    공통으로 사용할 ProgressIndicator 추가

---
+++ core/designsystem/src/main/java/com/juco/designsystem/component/loading/SubManagerLoadingBar.kt
@@ -0,0 +1,30 @@
+package com.juco.designsystem.component.loading
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.juco.designsystem.theme.SubManagerTheme
+
+@Composable
+fun SubManagerLoadingBar(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.3f))
+            .clickable(enabled = false) {}
+        ,
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            color = SubManagerTheme.colors.primaryText
+        )
+    }
+}
