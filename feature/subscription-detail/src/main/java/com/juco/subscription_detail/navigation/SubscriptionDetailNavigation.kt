a8d9be5ea07b190927cb000550c1fc7195a9f85c
Juseonghyun <jucoding99@gmail.com>
2026-01-10 16:26:23 +0900

    Detail Navigation 설정

--- feature/subscription-detail/src/main/java/com/juco/subscription_detail/navigation/SubscriptionDetailNavigation.kt
+++ feature/subscription-detail/src/main/java/com/juco/subscription_detail/navigation/SubscriptionDetailNavigation.kt
@@ -5,21 +5,24 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.juco.common.navigation.RouteModel
 import com.juco.subscription_detail.SubscriptionDetailRoute
 
-fun NavController.navigateToSubscriptionDetail(navOptions: NavOptions) {
-    navigate(RouteModel.SubscriptionDetail, navOptions)
+fun NavController.navigateToSubscriptionDetail(subId: Long, navOptions: NavOptions) {
+    navigate(RouteModel.SubscriptionDetail(subId), navOptions)
 }
 
 fun NavGraphBuilder.subscriptionDetailNavGraph(
     padding: PaddingValues,
     onPopBackStack: () -> Unit
 ) {
-    composable<RouteModel.SubscriptionDetail> {
+    composable<RouteModel.SubscriptionDetail> { backStackEntry ->
+        val subId = backStackEntry.toRoute<RouteModel.SubscriptionDetail>().subId
         SubscriptionDetailRoute(
             padding = padding,
-            onPopBackStack = onPopBackStack
+            onPopBackStack = onPopBackStack,
+            subId = subId
         )
     }
 }
