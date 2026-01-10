5f8eace5a642a89979399dd5bc3284c727e7a718
Juseonghyun <jucoding99@gmail.com>
2026-01-10 20:39:33 +0900

    UiState success 타입 변경

--- feature/subscription-detail/src/main/java/com/juco/subscription_detail/state/SubscriptionDetailUiState.kt
+++ feature/subscription-detail/src/main/java/com/juco/subscription_detail/state/SubscriptionDetailUiState.kt
@@ -1,9 +1,9 @@
 package com.juco.subscription_detail.state
 
-import com.juco.local.model.Subscription
+import com.juco.subscription_detail.model.SubscriptionDetailInfo
 
 sealed interface SubscriptionDetailUiState {
     data object Loading : SubscriptionDetailUiState
-    data class Success(val subscription: Subscription) : SubscriptionDetailUiState
+    data class Success(val subscription: SubscriptionDetailInfo) : SubscriptionDetailUiState
     data object Error : SubscriptionDetailUiState
 }
