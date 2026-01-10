cc59fecb7b4d0e85bca45af1266e52a8d2ede992
Juseonghyun <jucoding99@gmail.com>
2026-01-10 20:33:06 +0900

    Detail 에서 사용할 UI 모델 추가

---
+++ feature/subscription-detail/src/main/java/com/juco/subscription_detail/model/SubscriptionInfo.kt
@@ -0,0 +1,15 @@
+package com.juco.subscription_detail.model
+
+data class SubscriptionDetailInfo(
+    val subId: Long? = null,
+    val name: String? = null,
+    val thumbnail: String? = null,
+    val price: Long? = null,
+    val totalAmount: Long? = null,
+    val paymentDay: String? = null,
+    val nextPaymentDate: String? = null,
+    val dDay: String? = null,
+    val paymentCycle: String? = null,
+    val description: String? = null,
+    val enableNotification: Boolean
+)
