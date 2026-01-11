8ca584f736ca89a5d23bc33cd72b52715ae67bbd
Juseonghyun <jucoding99@gmail.com>
2026-01-11 19:55:41 +0900

    Setting 모듈 추가 및 gradle 설정

---
+++ feature/setting/build.gradle.kts
@@ -0,0 +1,19 @@
+import com.juco.build_logic.dsl.setNameSpace
+
+plugins {
+    id("com.juco.build_logic.convention.feature")
+    id("com.juco.build_logic.convention.compose")
+    id("com.juco.build_logic.primitive.hilt")
+}
+
+android {
+    setNameSpace("feature.setting")
+}
+
+dependencies {
+    implementation(projects.core.designsystem)
+    implementation(projects.core.common)
+    implementation(projects.domain.local)
+
+    implementation(libs.androidx.navigation.compose)
+}
