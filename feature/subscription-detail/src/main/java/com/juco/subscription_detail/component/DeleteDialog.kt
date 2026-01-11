98181b9f40954b9f984efefdce0d133b73377c79
Juseonghyun <jucoding99@gmail.com>
2026-01-11 19:50:40 +0900

    DeleteDialog에 커스텀한 red값 추가

--- feature/subscription-detail/src/main/java/com/juco/subscription_detail/component/DeleteDialog.kt
+++ feature/subscription-detail/src/main/java/com/juco/subscription_detail/component/DeleteDialog.kt
@@ -2,6 +2,7 @@ package com.juco.subscription_detail.component
 
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,28 +23,30 @@ fun DeleteDialog(
         title = {
             Text(
                 text = "구독 삭제",
+                color = SubManagerTheme.colors.primaryText,
                 style = SubManagerTheme.typography.h3SemiBold
             )
         },
         text = {
             Text(
                 text = "정말 이 구독 정보를 삭제하시겠습니까?\n삭제된 데이터는 복구할 수 없습니다.",
+                color = SubManagerTheme.colors.primaryText,
                 style = SubManagerTheme.typography.b1Regular
             )
         },
         confirmButton = {
-            androidx.compose.material3.TextButton(
+            TextButton(
                 onClick = onConfirm
             ) {
                 Text(
                     text = "삭제",
                     style = SubManagerTheme.typography.b1SemiBold,
-                    color = androidx.compose.ui.graphics.Color.Red
+                    color = SubManagerTheme.colors.red
                 )
             }
         },
         dismissButton = {
-            androidx.compose.material3.TextButton(
+            TextButton(
                 onClick = onDismissRequest
             ) {
                 Text(
