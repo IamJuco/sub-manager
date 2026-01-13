fb759e6dcdca91f830347f3a371c9ef8290c5a1b
Juseonghyun <jucoding99@gmail.com>
2026-01-13 16:00:55 +0900

    수정 화면의 메모 수정 섹션 UI 개발

---
+++ feature/subscription-edit/src/main/java/com/juco/subscription_edit/component/DescriptionSection.kt
@@ -0,0 +1,52 @@
+package com.juco.subscription_edit.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.juco.designsystem.component.textfield.SubManagerTextField
+import com.juco.designsystem.theme.SubManagerTheme
+
+@Composable
+fun DescriptionSection(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = "메모 수정",
+            style = SubManagerTheme.typography.h3SemiBold,
+            color = SubManagerTheme.colors.primaryText
+        )
+
+        Spacer(Modifier.height(4.dp))
+
+        SubManagerTextField(
+            value = value,
+            onValueChange = { newValue ->
+                if (newValue.length <= 10) {
+                    onValueChange(newValue)
+                }
+            },
+            placeholder = "서비스 설명"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DescriptionSectionPreview() {
+    SubManagerTheme {
+        DescriptionSection(
+            value = "",
+            onValueChange = {}
+        )
+    }
+}
