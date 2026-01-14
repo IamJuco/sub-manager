d78c03dc6e4960389414a7bbbef8e55611acfa30
Juseonghyun <jucoding99@gmail.com>
2026-01-14 17:00:00 +0900

    Application class에서 WorkManager 초기화 및 manifest provider 설정

--- app/src/main/java/com/juco/submanager/app/SubManagerApplication.kt
+++ app/src/main/java/com/juco/submanager/app/SubManagerApplication.kt
@@ -5,12 +5,27 @@ import com.juco.submanager.BuildConfig
 import com.juco.submanager.app.util.ReleaseTree
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import com.juco.work.NotificationUtil
+import com.juco.work.SubscriptionWorkerUtil
 
 @HiltAndroidApp
-class SubManagerApplication : Application() {
+class SubManagerApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
     override fun onCreate() {
         super.onCreate()
         initTimber()
+        NotificationUtil.createNotificationChannel(this)
+        SubscriptionWorkerUtil.enqueueDailyWork(this)
     }
 
     private fun initTimber() {
