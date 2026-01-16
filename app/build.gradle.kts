1831d3ef9570d3143156b6f4f1390b96455d8edb
Juseonghyun <jucoding99@gmail.com>
2026-01-16 14:12:24 +0900

    Admob 라이브러리 추가 및 manifest 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins{
     id("com.juco.build_logic.convention.application")
     id("com.juco.build_logic.primitive.hilt")
@@ -5,8 +8,20 @@ plugins{
     alias(libs.plugins.firebase.crashlytics)
 }
 
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+
 android {
     namespace = "com.juco.submanager"
+    defaultConfig {
+        val admobAppId = localProperties.getProperty("ADMOB_APP_ID")
+            ?: "ca-app-pub-3940256099942544~3347511713"
+        manifestPlaceholders["ADMOB_APP_ID"] = admobAppId
+    }
+
     buildTypes {
         debug {
             isMinifyEnabled = false
@@ -26,6 +41,7 @@ android {
 
 dependencies {
     implementation(projects.feature.main)
+    implementation(projects.core.common)
     implementation(projects.data.local)
     implementation(projects.work)
 
@@ -34,6 +50,7 @@ dependencies {
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.crashlytics)
+    implementation(libs.play.services.ads)
 
     implementation(libs.androidx.appcompat)
     implementation(libs.timber)
--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -1,4 +1,6 @@
 import com.juco.build_logic.dsl.setNameSpace
+import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
     id("com.juco.build_logic.convention.feature")
@@ -7,8 +9,19 @@ plugins {
     alias(libs.plugins.gms.google.service)
 }
 
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+
 android {
     setNameSpace("feature.main")
+    defaultConfig {
+        val admobOpenId = localProperties.getProperty("ADMOB_OPEN_ID")
+            ?: "ca-app-pub-3940256099942544/9257395921"
+        buildConfigField("String", "ADMOB_OPEN_ID", "\"$admobOpenId\"")
+    }
 }
 
 dependencies {
@@ -26,6 +39,7 @@ dependencies {
     implementation(libs.firebase.config)
     implementation(libs.play.app.update)
     implementation(libs.play.app.update.ktx)
+    implementation(libs.play.services.ads)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
